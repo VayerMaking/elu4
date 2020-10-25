2d7352290cdbc70fe7181f0a57ff5c4026b073c0
VayerMaking <martin.vayer@gmail.com>
2020-10-25 15:40:29 +0200

    slide images mobile

--- main.js
+++ main.js
@@ -1,29 +1,95 @@
+if (navigator.msMaxTouchPoints) {
 
-var slideIndex = 1;
-showSlides(slideIndex);
+  $('#slider').addClass('ms-touch');
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  $('#slider').on('scroll', function() {
+    $('.slide-image').css('transform','translate3d(-' + (100-$(this).scrollLeft()/6) + 'px,0,0)');
+  });
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+} else {
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  var slider = {
+
+    el: {
+      slider: $("#slider"),
+      holder: $(".holder"),
+      imgSlide: $(".slide-image")
+    },
+
+    slideWidth: $('#slider').width(),
+    touchstartx: undefined,
+    touchmovex: undefined,
+    movex: undefined,
+    index: 0,
+    longTouch: undefined,
+
+    init: function() {
+      this.bindUIEvents();
+    },
+
+    bindUIEvents: function() {
+
+      this.el.holder.on("touchstart", function(event) {
+        slider.start(event);
+      });
+
+      this.el.holder.on("touchmove", function(event) {
+        slider.move(event);
+      });
+
+      this.el.holder.on("touchend", function(event) {
+        slider.end(event);
+      });
+
+    },
+
+    start: function(event) {
+      // Test for flick.
+      this.longTouch = false;
+      setTimeout(function() {
+        window.slider.longTouch = true;
+      }, 250);
+
+      // Get the original touch position.
+      this.touchstartx =  event.originalEvent.touches[0].pageX;
+
+      // The movement gets all janky if there's a transition on the elements.
+      $('.animate').removeClass('animate');
+    },
+
+    move: function(event) {
+      // Continuously return touch position.
+      this.touchmovex =  event.originalEvent.touches[0].pageX;
+      // Calculate distance to translate holder.
+      this.movex = this.index*this.slideWidth + (this.touchstartx - this.touchmovex);
+      // Defines the speed the images should move at.
+      var panx = 100-this.movex/6;
+      if (this.movex < 600) { // Makes the holder stop moving when there is no more content.
+        this.el.holder.css('transform','translate3d(-' + this.movex + 'px,0,0)');
+      }
+      if (panx < 100) { // Corrects an edge-case problem where the background image moves without the container moving.
+        this.el.imgSlide.css('transform','translate3d(-' + panx + 'px,0,0)');
+      }
+    },
+
+    end: function(event) {
+      // Calculate the distance swiped.
+      var absMove = Math.abs(this.index*this.slideWidth - this.movex);
+      // Calculate the index. All other calculations are based on the index.
+      if (absMove > this.slideWidth/2 || this.longTouch === false) {
+        if (this.movex > this.index*this.slideWidth && this.index < 2) {
+          this.index++;
+        } else if (this.movex < this.index*this.slideWidth && this.index > 0) {
+          this.index--;
+        }
+      }
+      // Move and animate the elements.
+      this.el.holder.addClass('animate').css('transform', 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)');
+      this.el.imgSlide.addClass('animate').css('transform', 'translate3d(-' + 100-this.index*50 + 'px,0,0)');
+
+    }
+
+  };
+
+  slider.init();
 }
