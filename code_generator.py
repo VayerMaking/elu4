93799e3966dd91482bccc181270258e521434b06
VayerMaking <36741716+VayerMaking@users.noreply.github.com>
2021-10-07 16:29:02 +0300

    documentation 😜

--- code_generator.py
+++ code_generator.py
@@ -1,17 +1,16 @@
+# a tool for mass addition of new books
 import os
 import natsort
 
 path = os.getcwd() + "/images/math_uchebnik_new" # last part is the folder u have the new images
 
-files = os.listdir(path)
+files = os.listdir(path) # make an array with all the filenames in the directory
 
-file = open("generated.txt", "a")
+file = open("generated.txt", "a") # write generated div elements in a file for copy/paste later
 
-files = natsort.natsorted(files)
-#print(files)
+files = natsort.natsorted(files) # sort files names to be in book order
 
-for f in files:
-	#print(f)
+for f in files: # make div elements for every filename in the directory
 	file.write(
 	"""<div class="swiper-slide">
 	<img data-src="images/math_pomagalo/""" + str(f) + """ "   class="swiper-lazy">
